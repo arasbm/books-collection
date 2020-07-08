1bf1a732afa956963dc0eca1bf873777e6fa0848
Zein Elrez <67756015+Zeinelrez@users.noreply.github.com>
2020-07-08 10:48:47 -0700

    Create BookList.java
    Added initial book name

---
+++ BookList.java
@@ -0,0 +1,18 @@
+import java.util.*; 
+
+public class BookList
+{   
+  public static void main(String[] args) 
+  { 
+    String[] names;
+    
+    names = new String[40]; 
+    
+    names[0] = "Indian Horse"; 
+    
+    // Add your favourite book here, and make sure to not use an Index that's already been used. 
+ 
+    System.out.println(Arrays.toString(names)); 
+    
+  } 
+}
