d1ae294a3e5c60500d7b6602e9c9af782644b58e
Uyen[Brianna] Pham <67333957+BriannaPham799@users.noreply.github.com>
2020-07-14 15:43:13 -0700

    Update BookList.java

--- BookList.java
+++ BookList.java
@@ -6,19 +6,21 @@ public class BookList
   { 
     String[] names = new String[40]; 
     
-    names[0] = "Indian Horse"; 
+    names[0] = "Harry Potter"; 
     names[1] = "This Book Loves You!";
     names[2] = "A Brief History Of Time";
     names[3] = "Another book";
     names[4] = "The Theory Of Everything"; 
     names[5] = "America Before"; 
     names[6] = "The Faith";
-    names[7] = "Harry Potter";
+    names[7] = "Let Me Tell You About Jasper...";
     names[8] = "American Gods";
     names[9] = "Warriors";
     names[10]= "2 States";
     names[11] = "Prison to Praise";
     names[12] = "The Lord Of The Rings";
+    names[13] = "The Crown";
+    names[14] = "The Elite";
     // Add your favourite book here, and make sure to not use an Index that's already been used. 
  
     System.out.println(Arrays.toString(names)); 
